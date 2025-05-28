ff6d3b9ce1bca0ed6419a5a5494d389672425b17
abhishek <abhishekpl8219@gmail.com>
2025-05-28 11:58:11 +0530

    Made some cosmetic changes -1.

--- qtify/src/components/Hero/Hero.jsx
+++ qtify/src/components/Hero/Hero.jsx
@@ -1,15 +1,17 @@
+import React from "react";
+import HeroImage from "../../Assets/vibrating-headphone.png";
 import styles from "./hero.module.css";
-import heroImage from "../../Assets/hero.svg";
-
 const Hero = () => {
   return (
-    <div className={styles.hero}>
-      <div>
-        <h1> 100 Thousand Songs, ad-free </h1>
-        <h1> Over thousands podcast episodes</h1>
-      </div>
-      <img src={heroImage} alt="Hero_Image" />
-    </div>
+    <>
+      <section className={styles.Hero}>
+        <div className={styles.HeroTitle}>
+          <h1>100 Thousand Songs, ad-free</h1>
+          <h1>Over thousands podcast episodes</h1>
+        </div>
+        <img className={styles.HeroImage} src={HeroImage} alt="headphone" />
+      </section>
+    </>
   );
 };
 
